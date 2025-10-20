1f60f6cb4bad55a500b4715ca788cb0fb05c72e4
“David <davida@equator.zone>
2025-10-20 09:18:58 +0300

    action for cdk deploy

--- teardown_all.sh
+++ teardown_all.sh
@@ -5,7 +5,7 @@ echo "🧹 Destroying all blueprints..."
 FOLDERS=(
   "00_foundation"
   "01_ai_healthcheck_api"
-  "01_ai_bedrock_api"
+  "02_ai_healthcheck_bedrock"
 )
 
 for DIR in "${FOLDERS[@]}"; do
